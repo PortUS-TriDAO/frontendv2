6ad03633c0dc70aa7bf79ac54ac1f4dd4834606f
袁俊亮 <yuanjunliang@192.168.2.8>
2023-08-15 00:49:31 +0800

    merge home vuew

--- doc/wagemi.md
+++ doc/wagemi.md
@@ -1,3 +1,4 @@
 # wagemi
 
 - [文档](https://wagmi.sh/core/actions/getAccount)
+- [设计稿](https://lanhuapp.com/web/#/item/project/stage?tid=385cf451-2083-43ef-9d37-7829deb31a83&pid=db16500a-8879-477d-bc78-a0e1e5ea1838)
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
-import './assets/main.css'
+// import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import './index.css'
 
 import App from './App.vue'
 import router from './router'
