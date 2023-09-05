220fbb8d2f71f0d16a3a5c1312dc7292ef7efa27
袁俊亮 <yuanjunliang@192.168.2.4>
2023-09-06 01:21:43 +0800

    update home page

--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -12,7 +12,7 @@
             </p>
             <span>• Efficient profit allocation through DeCPS</span>
           </div>
-          <button>Create</button>
+          <button @click="goToCreate">Create</button>
         </div>
         <div></div>
         <div class="h-box right-box">
@@ -24,7 +24,7 @@
             </p>
             <span>• Earnings are safe and transparent </span>
           </div>
-          <button>Referral</button>
+          <button @click="goToDistribute">Referral</button>
         </div>
       </div>
       <p class="h-desc">Monetize Your Influence Instantly</p>
@@ -122,16 +122,122 @@
     <!-- Why choose PortUS -->
     <div class="choose-box">
       <h2 class="title">Why choose PortUS</h2>
-      <div class="choose-content"></div>
+      <div class="choose-content">
+        <el-carousel arrow="always">
+          <el-carousel-item v-for="(item, index) in data" :key="index">
+            <swiper-item
+              :title1="item.title1"
+              :title2="item.title2"
+              :desc="item.desc"
+              :list="item.list"
+            ></swiper-item>
+          </el-carousel-item>
+        </el-carousel>
+      </div>
     </div>
   </div>
 </template>
 <script setup lang="ts">
 import { onMounted } from 'vue'
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 import { statistic } from '@/api'
+import SwiperItem from './components/SwiperItem.vue'
 
 const route = useRoute()
+const router = useRouter()
+
+interface ISwiperItem {
+  title1: string
+  title2: string
+  desc: string
+  list: Array<{
+    title: string
+    desc: string
+  }>
+}
+
+const data: ISwiperItem[] = [
+  {
+    title1: 'DeCPS features',
+    title2: '',
+    desc: `
+    Project owners can mint DeCPS equity NFT at their own liberty.
+    `,
+    list: [
+      {
+        title: 'INO',
+        desc: 'The minting income/ royalty income from transferring DeCPS NFTs can serve as a financing method for project owners.'
+      },
+      {
+        title: 'Deposit into Smart Contract',
+        desc: 'All in-app purchases by users is paid to the DeCPS equity smart contract, which is open and transparent, with data queryable at any time.'
+      },
+      {
+        title: 'Automatic Allocation',
+        desc: 'The income generated from the product (top- up/ads) is automatically allocated under the support of the NFT smart contract, without needing to wait for settlement periods.'
+      }
+    ]
+  },
+  {
+    title1: 'Portal for Web 2 Consumers',
+    title2: 'Automated Exchange Settlement Payment Interface',
+    desc: 'Partnership with multiple licensed payment institutions, that can provide automated exchange settlement payment interface. This will allow web 2 user can complete transactions without feeling any presence of web 3',
+    list: [
+      {
+        title: 'Frictionless Experience',
+        desc: 'Web 2 user can use their most familiar payment methods, without the need to download wallet or CEX apps'
+      },
+      {
+        title: 'Automated Exchange & Settlement',
+        desc: 'Upon receieveing fiat currency from web 2 user it can be automatically exchanged into crypto currency'
+      },
+      {
+        title: 'Bridge between web 2 and web 3',
+        desc: 'Through the automated exchange and settlement interface, weeb 2 user can cross the web 3 barrier seamlessly. Thus promoting web 2 user enter into web 3'
+      }
+    ]
+  },
+  {
+    title1: 'DID',
+    title2: '',
+    desc: 'The SBTs held in the DIDs of all participants on the platform serve as their identity credentials and equity credentials. All participants of the platform can use these to prove their qualifications and authority to engage in platform business activities.',
+    list: [
+      {
+        title: 'How to Acquire',
+        desc: 'Participants initiate applications based on proof of their participation in platform activities, or the platform business activities grant qualifications to the corresponding participants. After approval, platform tokens are paid to mint the SBTs.'
+      },
+      {
+        title: 'Basis for Incentives',
+        desc: 'The sole basis for rewarding players with tokens, an important means to eliminate malicious behaviors such as using scripts, bots and brushing fake volume.'
+      },
+      {
+        title: 'Trustless Community',
+        desc: 'Identity recognition based on SBTs makes the composition of the user community more clear and orderly, strengthening community functions and making content more authoritative.'
+      }
+    ]
+  },
+  {
+    title1: 'Private Domain Store',
+    title2: '',
+    desc: `
+    Each private domain KOL can display all of the distribution rights NFTs they have minted in a customized store for their fans/members to browse and download/participate at any time.
+    `,
+    list: [
+      {
+        title: 'Personalised App store',
+        desc: 'With the platform features supported by PortUS, everyone can open their own App Store, so their fans and guild members can browse personalized product recommendations'
+      },
+      {
+        title: 'Refined Management',
+        desc: 'The operation status of private domain store can be checked at any time on a fully-featured, data-rich dashboard, enabling private domain traffic masters to effectively manage them.'
+      },
+      {
+        title: 'Accumilative Reputation',
+        desc: 'As the transaction volume of the shops expands, their reputation will continuously accumulate and be clearly displayed through on-chain data, facilitating the effective expansion of business scale.'
+      }
+    ]
+  }
+]
 
 onMounted(async () => {
   const refer = route.query.refer
@@ -149,6 +255,13 @@ onMounted(async () => {
     window.location.href = redirectURL
   }
 })
+
+function goToCreate() {
+  router.push('/project/create')
+}
+function goToDistribute() {
+  router.push('/mine/distribution')
+}
 </script>
 <style lang="less" scoped>
 .page-home {
@@ -388,6 +501,13 @@ onMounted(async () => {
   .choose-box {
     height: 1169px;
     background-color: #06173a;
+    .choose-content {
+      margin-top: 60px;
+    }
+    /deep/.el-carousel__container {
+      width: 1400px;
+      height: 789px;
+    }
   }
 }
 </style>
---
+++ src/views/home/components/SwiperItem.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="page-swiper-item">
+    <h5>{{ props.title1 }}</h5>
+    <h5>{{ props.title2 }}</h5>
+    <p>
+      {{ props.desc }}
+    </p>
+    <div>
+      <ul>
+        <li v-for="(item, index) in props.list" :key="index">
+          <div>
+            <h5>{{ item.title }}</h5>
+            <div class="divider"></div>
+            <p>
+              {{ item.desc }}
+            </p>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+const props = defineProps({
+  title1: String,
+  title2: String,
+  desc: String,
+  list: Array<{
+    title: String
+    desc: String
+  }>
+})
+</script>
+<style lang="less" scoped>
+.page-swiper-item {
+  width: 1400px;
+  height: 789px;
+  background-color: #0e2654;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 70px 90px 80px 90px;
+  box-sizing: border-box;
+  > h5 {
+    font-size: 36px;
+    font-weight: 700;
+    line-height: 50px;
+    text-align: center;
+  }
+  > p {
+    color: #c9c9c9;
+    font-size: 20px;
+    font-weight: 400;
+    line-height: 36px;
+    text-align: center;
+    margin-top: 30px;
+  }
+  > div {
+    > ul {
+      list-style: none;
+      margin: 0;
+      padding: 0;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      width: 1222px;
+      margin-top: 70px;
+      > li {
+        padding: 40px;
+        box-sizing: border-box;
+        width: 390px;
+        height: 366px;
+        background-color: #06173a;
+        border-radius: 38px;
+        h5 {
+          font-size: 20px;
+          font-weight: 500;
+          line-height: 50px;
+          font-family: Roboto;
+        }
+        p {
+          font-size: 16px;
+          line-height: 44px;
+          color: #c9c9c9;
+        }
+        .divider {
+          width: 310px;
+          height: 1px;
+          background-color: #1d253e;
+          margin-top: 22px;
+          margin-bottom: 26px;
+        }
+      }
+    }
+  }
+}
+</style>
