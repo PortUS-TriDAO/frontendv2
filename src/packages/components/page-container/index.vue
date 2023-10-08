cd9514fc83323ecfb2fac28471f47d48f42dc68a
enpeng <steven.feng@klavi.ai>
2023-10-09 00:55:16 +0800

    feat: page-container

---
+++ src/packages/components/page-container/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <main class="page-container">
+    <div class="page-banner" v-if="$slots.banner"><slot name="banner" /></div>
+    <article>
+      <slot />
+    </article>
+  </main>
+</template>
+<style lang="less">
+.page-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: #f7f7f7;
+  flex: 1;
+  > article {
+    background: #ffffff;
+    flex-grow: 1;
+    width: var(--container-width);
+    padding-left: var(--container-padding-left);
+    padding-right: var(--container-padding-right);
+    padding-bottom: 30px;
+  }
+}
+</style>
--- src/packages/index.ts
+++ src/packages/index.ts
@@ -1,4 +1,7 @@
 import Button from './components/button';
+import PageContainer from './components/page-container/index.vue';
+
 export function initPackages(app: any) {
   app.component('p-button', Button);
+  app.component('page-container', PageContainer);
 }
