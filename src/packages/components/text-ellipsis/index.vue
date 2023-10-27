aa3fe415419ba1e80e3774eccc40a6e0126a1692
enpeng <steven.feng@klavi.ai>
2023-10-27 23:48:11 +0800

    feta: add text-ellipsis component

---
+++ src/packages/components/text-ellipsis/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <p
+    :class="[
+      'text-ellipsis',
+      {
+        'text-ellipsis--expand': !ellipsis,
+      },
+    ]"
+  >
+    <!-- <el-text class="w-150px mb-2" :truncated="ellipsis"><slot /></el-text> -->
+    <span><slot /></span>
+    <a @click="ellipsis = !ellipsis">
+      <span>{{ ellipsis ? 'See more' : 'See less' }}</span>
+      <el-icon v-if="ellipsis"><ArrowDownBold /></el-icon>
+      <el-icon v-else><ArrowUpBold /></el-icon>
+    </a>
+  </p>
+</template>
+<script lang="ts" setup>
+import { ref } from 'vue';
+const ellipsis = ref(true);
+</script>
+
+<style lang="less">
+.text-ellipsis {
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 12px;
+  > span {
+    display: inline-block;
+    max-width: 100%;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow: hidden;
+    font-size: 24px;
+    font-weight: 400;
+    letter-spacing: 0px;
+    line-height: 28px;
+    color: #000;
+    text-align: left;
+  }
+  > a {
+    flex-shrink: 0;
+    color: #2a82e4;
+    cursor: pointer;
+    display: inline-flex;
+    align-items: center;
+    flex-direction: row;
+    gap: 4px;
+  }
+  &.text-ellipsis--expand {
+    display: block;
+    > span {
+      display: inline;
+      white-space: wrap;
+    }
+  }
+}
+</style>
--- src/packages/index.ts
+++ src/packages/index.ts
@@ -1,6 +1,7 @@
 import Button from './components/button';
 import NumberInput from './components/number-input';
 import PageContainer from './components/page-container/index.vue';
+import TextEllipsis from './components/text-ellipsis/index.vue';
 import WalletButton from './components/wallet-button';
 
 export function initPackages(app: any) {
@@ -8,5 +9,6 @@ export function initPackages(app: any) {
     .component('p-button', Button)
     .component('p-wallet-button', WalletButton)
     .component('p-number-input', NumberInput)
+    .component('text-ellipsis', TextEllipsis)
     .component('page-container', PageContainer);
 }
