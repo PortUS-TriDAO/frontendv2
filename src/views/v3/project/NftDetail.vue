8227ced76a40bfd34687fad70c4b3ececee2468f
enpeng <steven.feng@klavi.ai>
2023-10-28 17:30:03 +0800

    chore: nft detail title

--- src/views/v3/project/NftDetail.vue
+++ src/views/v3/project/NftDetail.vue
@@ -2,8 +2,14 @@
   <page-container class="pg-nft-detail">
     <nft-contract-item v-if="data" :item="data" @onDetail="handleDetail" hideActions />
     <div class="detail-divider"></div>
+    <div class="list-title">NFT list</div>
     <div class="list">
-      <SkuItem v-for="item in nftList?.rows || []" :key="item.tokenId" :item="item" />
+      <SkuItem
+        v-for="item in nftList?.rows || []"
+        :key="item.tokenId"
+        :item="item"
+        @click="handleDetail(item.tokenId)"
+      />
     </div>
   </page-container>
 </template>
@@ -26,9 +32,8 @@ console.log('nftList=', nftList);
 
 // import bannerImg from './assets/banner.png';
 
-function handleDetail(businessId: string) {
-  const { id } = route.params;
-  router.push(`/project/${id}/${businessId}`);
+function handleDetail(tokenId: number) {
+  router.push(`/nft/${nftAddress}/${tokenId}`);
 }
 </script>
 <style lang="less" scoped>
@@ -37,10 +42,20 @@ function handleDetail(businessId: string) {
     margin: 20px 0;
     border-bottom: solid 1px rgba(0, 0, 0, 0.2);
   }
+  .list-title {
+    font-size: 24px;
+    font-weight: 700;
+    line-height: 28px;
+    color: #000;
+    margin: 0 0 14px 0;
+  }
   .list {
     display: flex;
     flex-direction: row;
     gap: 12px;
+    > div {
+      cursor: pointer;
+    }
   }
 }
 </style>
