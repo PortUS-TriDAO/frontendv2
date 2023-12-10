d7ea9b08c8de99e2c1fa02afd8e69e871621ffc6
xiaoyuan <1610712512@qq.com>
2023-12-10 14:06:46 +0800

    add upload dialog

---
+++ doc.md
@@ -0,0 +1,11 @@
+## WhitelistRights
+
+setWhitelist
+claimWhitelist ？页面哪个地方领
+airdrop
+mint
+
+## RetailerDeployer
+
+createUnmintedRetailer
+nftType: // 0: 721 1: 1155
--- src/views/projects/DeployAltc.vue
+++ src/views/projects/DeployAltc.vue
@@ -1,43 +1,70 @@
 <template>
   <main-content>
     <div class="page-deploy-altc">
-      <h3 class="title">Deploy ALTC</h3>
-      <div class="form-box">
-        <el-form label-position="top" :inline="true">
-          <el-form-item label="Deploy ALTC">
-            <el-input></el-input>
-          </el-form-item>
-          <el-form-item label="Percent for Agents">
-            <el-input></el-input>
-          </el-form-item>
-          <el-form-item label="Deploy Intro">
-            <el-input></el-input>
-          </el-form-item>
-          <el-form-item label="Pay token">
-            <el-input></el-input>
-          </el-form-item>
-          <el-form-item label="ALTC Description">
-            <el-input></el-input>
-          </el-form-item>
-          <el-form-item label="Right Quantity">
-            <el-input></el-input>
-          </el-form-item>
-        </el-form>
-      </div>
-      <div class="blockchain">
-        <h5>BlockChain</h5>
-        <div class="chain-box">
-          <span>Ethereum</span>
+      <h3 class="title">Deploy ALTC (Agent Licence Token Contract)</h3>
+      <div class="top-box">
+        <div class="blockchain">
+          <h5>BlockChain</h5>
+          <div class="chain-box">
+            <span>Ethereum</span>
+          </div>
+        </div>
+
+        <div class="contract-type">
+          <h5>Contract Type</h5>
+          <div class="contract-type-box">ALTC</div>
         </div>
       </div>
-
-      <div class="contract-type">
-        <h5>Contract Type</h5>
-        <div class="contract-type-box">ALTC</div>
-      </div>
-
-      <div class="save-btn">
-        <p-button>Save</p-button>
+      <div class="form-box">
+        <el-form
+          ref="ruleFormRef"
+          :model="ruleForm"
+          :rules="rules"
+          label-position="top"
+          :inline="true"
+        >
+          <el-form-item label="ALTC Name" prop="contractName">
+            <el-input v-model="ruleForm.contractName"></el-input>
+          </el-form-item>
+          <el-form-item label="Percent for Agents" prop="sharePercentage">
+            <el-input
+              v-model="ruleForm.sharePercentage"
+              :max="100"
+              oninput="value=Number(value.replace(/[^0-9.]/g, '')) > 100 ? 100 : Number(value.replace(/[^0-9.]/g, ''))"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="Deploy Intro" prop="briefIntro">
+            <el-input placeholder="100 characters used" v-model="ruleForm.briefIntro"></el-input>
+          </el-form-item>
+          <el-form-item label="Pay token" prop="payToken">
+            <el-select class="item-select" v-model="ruleForm.payToken">
+              <el-option
+                v-for="item in SelectTokenList"
+                :key="item.label"
+                :label="item.label"
+                :value="item.value"
+              >
+              </el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="ALTC Description" prop="description">
+            <el-input placeholder="description" v-model="ruleForm.description"></el-input>
+          </el-form-item>
+          <el-form-item label="Right Quantity" prop="rightQuantity">
+            <el-input
+              placeholder="Right quantity"
+              v-model="ruleForm.rightQuantity"
+              oninput="value=value.replace(/[^0-9.]/g, '')"
+            ></el-input>
+          </el-form-item>
+          <el-form-item style="width: 100%">
+            <div class="save-btn">
+              <p-wallet-button :loading="loading" class="btn" @click="handleSubmit(ruleFormRef)"
+                >Deploy
+              </p-wallet-button>
+            </div>
+          </el-form-item>
+        </el-form>
       </div>
 
       <!-- Drop Setting -->
@@ -46,15 +73,22 @@
         <!-- White List -->
         <div class="drop-item">
           <div class="title">
-            <el-switch></el-switch>
+            <el-switch v-model="allowWhiteList"></el-switch>
             <h6>White list</h6>
           </div>
           <el-form label-position="top" :inline="true">
             <el-form-item label="Numbers of items">
-              <el-input></el-input>
+              <el-input
+                v-model="dropSettingForm.whiteListCount"
+                oninput="value=value.replace(/[^0-9.]/g, '')"
+              ></el-input>
             </el-form-item>
             <el-form-item label="Mint start date & time">
-              <el-input></el-input>
+              <el-date-picker
+                v-model="dropSettingForm.whiteListStartTime"
+                type="datetime"
+                placeholder="Select date and time"
+              />
             </el-form-item>
           </el-form>
         </div>
@@ -62,12 +96,16 @@
         <!-- Free mint -->
         <div class="drop-item">
           <div class="title">
-            <el-switch></el-switch>
+            <el-switch v-model="allowFreeMint"></el-switch>
             <h6>Free Mint</h6>
           </div>
           <el-form label-position="top" :inline="true">
             <el-form-item label="Mint start time">
-              <el-input></el-input>
+              <el-date-picker
+                v-model="dropSettingForm.freeMintStartTime"
+                type="datetime"
+                placeholder="Select date and time"
+              />
             </el-form-item>
           </el-form>
         </div>
@@ -75,30 +113,137 @@
         <!-- Airdrop -->
         <div class="drop-item">
           <div class="title">
-            <el-switch></el-switch>
+            <el-switch v-model="allowAirdrop"></el-switch>
             <h6>Airdrop</h6>
           </div>
           <p style="margin-left: 20px">Add address in My profile</p>
-          <!-- <el-form label-position="top" :inline="true">
-            <el-form-item label="Numbers of items">
-              <el-input></el-input>
-            </el-form-item>
-            <el-form-item label="Mint start date & time">
-              <el-input></el-input>
-            </el-form-item>
-          </el-form> -->
         </div>
 
         <div class="save-btn">
-          <p-button>Deploy</p-button>
+          <p-wallet-button :loading="dropSettingLoading" @click="setDropSetting"
+            >Save
+          </p-wallet-button>
         </div>
       </div>
+
+      <!--  test   -->
+      <upload-white-list-card :visible="visible"></upload-white-list-card>
+      <el-button type="primary" @click="visible = !visible">show upload</el-button>
     </div>
   </main-content>
 </template>
 
 <script lang="ts" setup>
+import { type Address, waitForTransaction } from '@wagmi/core';
+import type { FormInstance } from 'element-plus';
+import { ElMessage } from 'element-plus';
+import { reactive, ref, toRaw } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+
 import MainContent from '@/components/MainContent.vue';
+import UploadWhiteListCard from '@/components/UploadWhiteListCard.vue';
+import { SelectTokenList } from '@/constant/contracts';
+import { useProjectStore } from '@/stores/useProject';
+import { useWhiteListRightsContract } from '@/stores/useWhiteListRightsContract';
+import { deployAltcRules } from '@/views/projects/rules';
+
+const router = useRouter();
+const route = useRoute();
+const projectStore = useProjectStore();
+const whiteListRightsContract = useWhiteListRightsContract();
+const projectId: string = route.params.projectId as string;
+const loading = ref(false);
+const allowWhiteList = ref(false);
+const allowFreeMint = ref(false);
+const allowAirdrop = ref(false);
+const dropSettingLoading = ref(false);
+
+const visible = ref(false);
+
+const ruleFormRef = ref<FormInstance>();
+const ruleForm = reactive({
+  contractName: '',
+  symbol: '',
+  sharePercentage: '',
+  briefIntro: '',
+  payToken: '',
+  description: '',
+  rightQuantity: '',
+});
+
+const dropSettingForm = reactive({
+  whiteListCount: 0,
+  whiteListStartTime: 0,
+  freeMintStartTime: 0,
+});
+
+const rules = reactive(deployAltcRules);
+
+async function handleSubmit(formEl: FormInstance | undefined) {
+  if (!formEl) return;
+  await formEl.validate((valid, fields) => {
+    if (valid) {
+      createProject();
+    } else {
+      console.log('error submit', fields);
+    }
+  });
+}
+
+async function setDropSetting() {
+  try {
+    dropSettingLoading.value = true;
+    const { whiteListCount, freeMintStartTime, whiteListStartTime } = toRaw(dropSettingForm);
+    const tx = await whiteListRightsContract.setDropSetting(
+      freeMintStartTime,
+      whiteListStartTime,
+      whiteListCount,
+    );
+    await waitForTransaction(tx);
+    ElMessage.success('setting success');
+  } catch (e) {
+    ElMessage.error('setting failed');
+  } finally {
+    dropSettingLoading.value = false;
+  }
+}
+
+const createProject = async () => {
+  const {
+    contractName,
+    sharePercentage,
+    symbol,
+    payToken,
+    rightQuantity,
+    briefIntro,
+    description,
+  } = toRaw(ruleForm);
+  const token = payToken as Address;
+  try {
+    loading.value = true;
+
+    // TODO: percentage
+    const { success, data } = await projectStore.createProject({
+      projectId,
+      briefIntro,
+      description,
+      contractName,
+      symbol,
+      payToken: token,
+      rightQuantity,
+      sharePercentage,
+    });
+
+    if (!success) throw new Error(data);
+    ElMessage.success('create project success');
+    router.push(`/project/submitsuccess/${projectId}/${data.bizId}`);
+  } catch (e) {
+    console.error('create project fail', e);
+    ElMessage.error('create project fail');
+  } finally {
+    loading.value = false;
+  }
+};
 </script>
 
 <style lang="less" scoped>
@@ -109,43 +254,57 @@ import MainContent from '@/components/MainContent.vue';
     margin-bottom: 14px;
     padding: 20px;
   }
+
+  .top-box {
+    display: flex;
+    flex-direction: row;
+
+    .blockchain {
+      // height: 389px;
+      padding: 20px;
+      box-sizing: border-box;
+
+      .chain-box {
+        width: 437px;
+        line-height: 220px;
+        background: url('@/assets/images/chain-bg.jpg') center center no-repeat;
+        background-size: contain;
+
+        > span {
+          font-size: 32px;
+          color: rgba(35, 115, 192, 1);
+          margin-left: 24px;
+          font-weight: 700;
+        }
+      }
+    }
+
+    .contract-type {
+      padding: 20px;
+
+      .contract-type-box {
+        width: 220px;
+        line-height: 50px;
+        text-align: center;
+        border: solid 1px #ccc;
+      }
+    }
+  }
+
+  .save-btn {
+    display: flex;
+    justify-content: center;
+    width: 100%;
+  }
+
   .title {
     margin: 20px;
     font-size: 36px;
   }
-  .blockchain {
-    // height: 389px;
-    padding: 20px;
-    box-sizing: border-box;
-    .chain-box {
-      width: 437px;
-      line-height: 220px;
-      background: url('@/assets/images/chain-bg.jpg') center center no-repeat;
-      background-size: contain;
-      > span {
-        font-size: 32px;
-        color: rgba(35, 115, 192, 1);
-        margin-left: 24px;
-        font-weight: 700;
-      }
-    }
-  }
-  .contract-type {
-    padding: 20px;
-    .contract-type-box {
-      width: 220px;
-      line-height: 50px;
-      text-align: center;
-      border: solid 1px #ccc;
-    }
-  }
-  .save-btn {
-    display: flex;
-    justify-content: center;
-  }
 
   .drop-setting {
     box-sizing: border-box;
+
     .drop-item {
       h6 {
         display: inline-block;
@@ -154,10 +313,15 @@ import MainContent from '@/components/MainContent.vue';
     }
   }
 }
+
 .el-form-item {
   width: 440px;
   margin: 20px;
   font-size: 24px;
   font-weight: 700;
+}
+
+.el-select {
+  width: 100%;
 }
 </style>
