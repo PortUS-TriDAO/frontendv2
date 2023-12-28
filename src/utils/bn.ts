2ef3d2e97d1b7acbfd6149aef2863537700a8bbd
enpeng <steven.feng@klavi.ai>
2023-12-28 17:59:16 +0800

    chore:  临时配置生产环境Decimals为6位

--- src/utils/bn.ts
+++ src/utils/bn.ts
@@ -7,6 +7,13 @@ export function toBN(n: BigNumberLike) {
   return new BN(n);
 }
 
-export function extendsDecimals(num: BigNumberLike, decimals: number = 18): BN {
-  return toBN(num).multipliedBy(toBN(10).pow(decimals));
+const isProd = ['www.portus.world', 'portus.world'].includes(window.location.host);
+
+export function extendsDecimals(num: BigNumberLike): BN {
+  // TODO: 临时配置生产环境Decimals为6位，之后在订正为配置
+  return toBN(num).multipliedBy(toBN(10).pow(isProd ? 6 : 18));
 }
+// export function extendsDecimals(num: BigNumberLike, decimals: number = 18): BN {
+//   // TODO: 临时配置生产环境Decimals为6位，之后在订正为配置
+//   return toBN(num).multipliedBy(toBN(10).pow(decimals));
+// }
